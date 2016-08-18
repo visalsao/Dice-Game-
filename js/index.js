588463104a7e505c8f990223f1f4ea5badc2c13e
visalsao <visal.s@ligercambodia.org>
2016-08-18 17:02:28 +0700

    Try with new computer

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
-var ran = Math.random();
-if (ran < 1/6) {
-	alert("1");
-} else if (ran > 1/6 && ran < 2/6) {
-	alert("2");
-} else if (ran > 2/6 && ran < 3/6) {
-	alert("3");
-} else if (ran > 3/6 && ran < 4/6) {
-	alert("4");
-} else if (ran > 4/6 && ran < 5/6) {
-	alert("5");
-} else if (ran > 5/6 && ran < 1) {
-	alert("6");
-}
+$(document).ready(function() {
+	$("#intro").show();
+	//Choocing game
+    $("#play").bind("click", function(event) {
+        $("#chooseGameMode").show();
+    });
+    //One player
+    $("#onePlayer").bind("click", function(event){
+    	$("#strOne").show();
+    	$("#intro").hide();
+    	$("#chooseGameMode").hide();
 
-$( document ).ready( function( ){
-	$("#play").blind( "click", function(event) {
-		$("chooseGameMode").show();
-	})
+    });
+
+    $("#playOne").bind("click", function(event){
+
+    });
 });
+//Visal 
+//JS
+function name(){
+	alert("user");
+}
